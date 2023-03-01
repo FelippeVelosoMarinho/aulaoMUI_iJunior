385415ba08f785fce26f08839b39f193d89667b0
Felippe Veloso <60450622+FelippeVelosoMarinho@users.noreply.github.com>
2023-02-28 23:21:51 -0300

    feat: definição de paleta e tipografia

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,37 @@
+import { ThemeProvider } from "@emotion/react";
+import { createTheme, CssBaseline } from "@mui/material";
 import Login from "./pages/Login";
 
 function App() {
-  return <div className="App">
-    <Login />
-  </div>;
+  const theme = createTheme({
+    palette: {
+      primary: {
+        main: "#F9A006",
+        light: "#FFF",
+      },
+      secondary: {
+        main: "#E60882",
+      },
+      background: {
+        default: "#F0F0F0",
+      },
+      contrastThreshold: 2,
+    },
+    typography: {
+      fontFamily: ["Poppins", "sans-serif"].join(","),
+      fontWeightLight: 300,
+      fontWeightRegular: 400,
+      fontWeightMedium: 500,
+      fontWeightBold: 700,
+    },
+  });
+
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Login />
+    </ThemeProvider>
+  );
 }
 
 export default App;
